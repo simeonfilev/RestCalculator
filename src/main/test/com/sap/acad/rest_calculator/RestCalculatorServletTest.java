9a455bb22d15588645e13c082fa68bf01241240d
Simeon <simeontfilev@gmail.com>
2020-04-24 13:21:28 +0300

    refactoring and headers->parameters

--- src/main/test/com/sap/acad/rest_calculator/RestCalculatorServletTest.java
+++ src/main/test/com/sap/acad/rest_calculator/RestCalculatorServletTest.java
@@ -1,59 +1,97 @@
 package com.sap.acad.rest_calculator;
 
 import com.sap.acad.calculator.Calculator;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.mockito.Mockito;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
 
-import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.WriteListener;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.*;
 
 
 public class RestCalculatorServletTest {
 
-    @Test
-    public void restAPIIsWorkingCorrectly() throws IOException, ServletException {
-        HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
-        HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
-        Map<String, String> headers = new HashMap<>();
-        headers.put("expression", "2*3");
-        Enumeration<String> headerNames = Collections.enumeration(headers.keySet());
-        Mockito.when(request.getHeaderNames()).thenReturn(headerNames);
-        Mockito.doAnswer(new Answer<String>() {
+    private static Calculator calculator;
+    private static HttpServletResponse response;
+    private static HttpServletRequest request;
+    private static RestCalculatorServlet restServlet;
+    private static String outputJSON;
+
+    @BeforeAll
+    static void init() throws IOException {
+        calculator = new Calculator();
+        request = Mockito.mock(HttpServletRequest.class);
+        response = Mockito.mock(HttpServletResponse.class);
+        restServlet = new RestCalculatorServlet();
+        outputJSON = "";
+        ServletOutputStream servletOutputStream = new ServletOutputStream() {
             @Override
-            public String answer(InvocationOnMock invocation) throws Throwable {
-                Object[] args = invocation.getArguments();
-                return headers.get((String) args[0]);
+            public boolean isReady() {
+                return false;
             }
-        }).when(request).getHeader("expression");
-        ArrayList<Integer> arrayList = new ArrayList<>();
-         ServletOutputStream servletOutputStream = new ServletOutputStream() {
-           @Override
-           public boolean isReady() {
-               return false;
-           }
 
-           @Override
-           public void setWriteListener(WriteListener writeListener) {
-           }
+            @Override
+            public void setWriteListener(WriteListener writeListener) {
+            }
 
-           @Override
-           public void write(int b) throws IOException {
-                arrayList.add(b);
-           }
-       };
-
+            @Override
+            public void write(int b) throws IOException {
+                outputJSON += (char) b;
+            }
+        };
         Mockito.when(response.getOutputStream()).thenReturn(servletOutputStream);
-        RestCalculatorServlet testServlet = new RestCalculatorServlet();
-        testServlet.doGet(request,response);
-        Assertions.assertTrue(arrayList.size()>2); // more than empty response {}
     }
 
+    @AfterEach
+    public void clear() {
+        outputJSON = "";
+    }
+
+    public String removeJSONSpacings(String json) {
+        json = json.replaceAll("\n", "")
+                .replaceAll("\r", "")
+                .replaceAll("\"", "");
+        return json;
+    }
+
+    public String getAnswerParameterFromJSON() {
+        String answerFromServletRequest = removeJSONSpacings(outputJSON.substring(outputJSON.indexOf("answer: ") + "answer: ".length()));
+        String answerToExpressionFromServletRequest = answerFromServletRequest.replaceAll(",", "")
+                .replaceAll("}", "");
+        return answerToExpressionFromServletRequest;
+    }
+
+    @Test
+    public void returnsJSONToServletRequest() throws IOException {
+        String expressionToCalculate = "5+2*3";
+        Mockito.when(request.getParameter("expression")).thenReturn(expressionToCalculate);
+        restServlet.doGet(request, response);
+        outputJSON = removeJSONSpacings(outputJSON);
+        boolean correctJSONFormat = outputJSON.charAt(0) == '{' && outputJSON.charAt(outputJSON.length() - 1) == '}';
+        Assertions.assertTrue(correctJSONFormat);
+    }
+
+    @Test
+    public void returnsCorrectAnswerToExpression() throws IOException {
+        String expressionToCalculate = "5+2*3";
+        Double expectedAnswerToExpression = calculator.calculate(expressionToCalculate);
+        Mockito.when(request.getParameter("expression")).thenReturn(expressionToCalculate);
+        restServlet.doGet(request, response);
+        outputJSON = removeJSONSpacings(outputJSON);
+        String answerFromServlet = getAnswerParameterFromJSON();
+        boolean correctAnswerToExpression = answerFromServlet.equals(String.valueOf(expectedAnswerToExpression));
+        Assertions.assertTrue(correctAnswerToExpression, "Wrong answer!");
+    }
+
+    @Test
+    public void returnsCorrectAnswerToEmptyExpression() throws IOException {
+        String expressionToCalculate = "";
+        Mockito.when(request.getParameter("expression")).thenReturn(expressionToCalculate);
+        restServlet.doGet(request, response);
+        outputJSON = removeJSONSpacings(outputJSON);
+        boolean correctAnswerToExpression = outputJSON.equals(String.valueOf("{}"));
+        Assertions.assertTrue(correctAnswerToExpression, "Not empty!");
+    }
 }
