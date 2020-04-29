f3ea55d845b9a17d0182f51743913caba4dba62d
Simeon <simeontfilev@gmail.com>
2020-04-29 19:51:58 +0300

    fixed test

--- src/main/test/com/sap/acad/rest_calculator/IntegrationTest.java
+++ src/main/test/com/sap/acad/rest_calculator/IntegrationTest.java
@@ -6,17 +6,24 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+import com.sap.acad.calculator.Calculator;
 import org.apache.catalina.startup.Tomcat;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
+import javax.ws.rs.core.MediaType;
+
 public class IntegrationTest {
     private Tomcat tomcat;
+    private Calculator calculator;
 
     @Before
     public void setUp() throws Exception {
+        this.calculator = new Calculator();
         tomcat = new Tomcat();
         tomcat.setPort(8085);
         tomcat.getConnector();
@@ -35,11 +42,20 @@ public class IntegrationTest {
         tomcat.stop();
         Assertions.assertFalse(tomcat.getServer().getState().isAvailable());
         tomcat.destroy();
-    }
 
+    }
+    private boolean isValidJSON(String test) {
+        try {
+            new JSONObject(test);
+        } catch (JSONException ex) {
+            return false;
+        }
+        return true;
+    }
     @Test
     public void verifyRESTServiceCorrectness() throws IOException, InterruptedException {
-        String url = "http://localhost:8085/RestCalculator/calculator/expressions?expression=5";
+        String expression="5-3";
+        String url = "http://localhost:8085/RestCalculator/calculator/expressions?expression="+expression;
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url))
@@ -48,6 +64,12 @@ public class IntegrationTest {
 
         HttpResponse<String> response = client.send(request,
                 HttpResponse.BodyHandlers.ofString());
+        String json = response.body();
+        Assertions.assertTrue(isValidJSON(json), "Does not return valid JSON");
+        Double expectedAnswerToExpression = calculator.calculate(expression);
+        JSONObject jsonObj = new JSONObject(json);
+        Assertions.assertEquals(expectedAnswerToExpression, jsonObj.getDouble("answer"));
         Assertions.assertEquals(200, response.statusCode());
+        Assertions.assertEquals(MediaType.APPLICATION_JSON,response.headers().firstValue("content-type").get());
     }
 }
